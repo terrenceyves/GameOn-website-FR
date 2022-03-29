9d8710c0deed9e525229f315663ae921ae64e57d
Annaelle <annaelleemery512@gmail.com>
2022-03-29 22:35:15 +0200

    Input Validations

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,6 +15,22 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModalBtn = document.querySelectorAll(".close");
 
+var firstname = document.reserve.first;
+var lastname = document.reserve.last;
+var mail = document.reserve.email;
+var birth = document.reserve.birthdate;
+var qty = document.reserve.quantity;
+var locations = document.reserve.location;
+var generalConditions = document.reserve.checkbox1;
+
+var error_first = document.getElementById("error-first");
+var error_last = document.getElementById("error-last");
+var error_email = document.getElementById("error-email");
+var error_birth = document.getElementById("error-birth");
+var error_qty = document.getElementById("error-qty");
+var error_option = document.getElementById("error-option");
+var error_conditions = document.getElementById("error-conditions");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -32,7 +48,7 @@ function closeModal() {
 }
 
 // validation du mail
-function validateMail() {
+function validateRegexMail() {
   var mail = document.reserve.email;
   var mailFormat = /^[\w-.]+@([\w-]+\.)+[\w-]{2,3}$/;
 
@@ -45,70 +61,100 @@ function validateMail() {
 
 
 
+// Validations des champs
+function validateFirst() {
+  if(firstname.value == "" || firstname.value.length < 2) {
+    error_first.innerHTML = "Veuillez entrer 2 caractères ou plus";
+    firstname.focus();
+    return false;
+  } else {
+    error_first.innerHTML = "";
+    return true;
+  }
+}
+
+function validateLast() {
+  if(lastname.value == "" || lastname.value.length < 2) {
+    error_last.innerHTML = "Veuillez entrer 2 caractères ou plus";
+    firstname.focus();
+    return false;
+  } else {
+    error_last.innerHTML = "";
+    return true;
+  }
+}
+
+function validateEmail() {
+    //si la valeur du champ email est vide
+    if(mail.value == "") {
+      mail.focus();
+      return false;
+    }
+  
+    //si la valeur du champ email ne match pas avec mailFormat
+    else if(validateRegexMail() == false) {
+      error_email.innerHTML = "Veuillez entrer un email valide";
+      mail.focus();
+      return false;
+    } else {
+      return true;
+    }
+}
+
+function validateBirth() {
+  //si la valeur du champ birthdate est vide
+  if(birth.value == "") {
+    error_birth.innerHTML = "Veuillez entrer votre date de naissance";
+    birth.focus();
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validateQty() {
+  //si la valeur du champ quantity est vide
+  if(qty.value == "") {
+    error_qty.innerHTML = "Veuillez entrer le nombre de tournois auquels vous avez participé";
+    qty.focus();
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validateOptions() {
+  if(locations.value == '' ) {
+    error_option.innerHTML = "Veuillez choisir une option";
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function validateConditions() {
+  if(!generalConditions.checked) {
+    error_conditions.innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions";
+    return false;
+  } else {
+    return true;
+  }
+}
+
 
 // fonction valider
 function validate() {
 
-  var firstname = document.reserve.first;
-  var lastname = document.reserve.last;
-  var mail = document.reserve.email;
-  var birth = document.reserve.birthdate;
-  var qty = document.reserve.quantity;
-  var locations = document.reserve.location;
-  var generalConditions = document.reserve.checkbox1;
+if (validateFirst() == true && validateLast() == true && validateEmail() == true && validateBirth() == true && validateQty() == true && validateOptions() == true && validateConditions() == true) {
+  alert("Votre demande a bien été envoyée");
+  return true;
+} else {
+  return false;
+}
 
-  //si la valeur du champ prénom est vide
-  if(firstname.value == "" || firstname.value.length < 2) {
-    firstname.focus();
-    return false;
-  }
 
-  //si la valeur du champ nom est vide
-  if(lastname.value == "" || lastname.value.length < 2) {
-    lastname.focus();
-    return false;
-  }
-
-  //si la valeur du champ email est vide
-  if(mail.value == "") {
-    mail.focus();
-    return false;
-  }
-
-  //si la valeur du champ email ne match pas avec mailFormat
-  if(validateMail() == false) {
-    console.log(validateMail());
-    alert("Vous avez entré un mail invalide")
-    mail.focus();
-    return false;
-  }
-
-  //si la valeur du champ birthdate est vide
-  if(birth.value == "") {
-    alert("Veuillez entrer votre date de naissance")
-    birth.focus();
-    return false;
-  }
-
-  //si la valeur du champ quantity est vide
-  if(qty.value == "") {
-    alert("Veuillez saisir le nombre de tournois auquel vous avez participé")
-    qty.focus();
-    return false;
-  }
-
-  if(locations.value == '' ) {
-    alert("Veuillez choisir un tournoi")
-    return false;
-  }
-
-  if(!generalConditions.checked) {
-    alert("Veuillez cocher la case \"J'ai lu et accepté les conditions d'utilisation.\"")
-    return false;
-  }
-
-  else {
-    alert("OK !!!");
-    return true;
-  }
+  // var error_last = document.querySelector(".error-last");
+  // var error_birth = document.querySelector(".error-birth");
+  // var error_option = document.querySelector(".error-option");
+  // var error_conditions = document.querySelector(".error-conditions");
 }
